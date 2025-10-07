95358c9b4f53cbfba3b6590190e27a7f2d3c339a
Dushyna <olgavnv777@gmail.com>
2025-10-07 18:28:32 +0200

    Status

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 const library = [];
-// isbn, title, author, year
-addBook.onclick = function () {
-    // TODO task 1 get data from inputs, and put book in <ol id="result"></ol>
-    // check is book unique
 
-    if (findBook(library, isbn.value.trim()) === -1) {
+addBook.onclick = function () {
+    if (findBook(library, isbn.value.trim()) === -1 && isbn.value.trim() !== "") {
         const book = new Book(isbn.value.trim(), title.value.trim(), author.value.trim(), year.value);
         library.push(book);
         const li = document.createElement("li");
-        const btnDel = document.createElement('button');
-        btnDel.append("X");
-        btnDel.style.color = "red";
-        btnDel.style.marginLeft = "5px";
-        // btnDel.addEventListener("click", function (e) {
-        //     const isbnn = e.target.parentElement.innerText.split(",")[0].substring(6);
-        //     library.splice(findBook(library, isbnn), 1);
-        //     e.target.parentElement.remove();
-        // });
-        btnDel.addEventListener("click", (e) => {
+        const btnDel = createButtonDel(() => {
             library.splice(findBook(library, book.isbn), 1);
-            e.target.parentElement.remove();
+            statusb();
         });
         li.append(book.toString(), btnDel);
         result.append(li);
+        statusb();
 
+
+    } else {
+        alert(`Book with isbn = ${isbn.value} exists or ISBN is absent`);
     }
-    isbn.value = "";
-    title.value = "";
-    author.value = "";
-    year.value = "";
+    isbn.value = title.value = author.value = year.value = '';
 
-    // TODO task 2 advanced: implements remove book functionality
-    // NB! after remove book, we have possibility to add book with same isbn
 }
 
 function findBook(library, isbn) {
---
+++ utils.js
@@ -0,0 +1,38 @@
+function createButtonDel(callback) {
+    const btnDel = document.createElement('button');
+    btnDel.append("X");
+    btnDel.style.color = "red";
+    btnDel.style.marginLeft = "5px";
+    btnDel.addEventListener("click", e => {
+        e.target.parentElement.remove();
+        if (typeof callback === "function") {
+            callback();
+        }
+    });
+    return btnDel;
+}
+
+function statusb() {
+    console.log(stats.children.length)
+    if (stats.children.length === 1) {
+        const pMin = document.createElement("p");
+        pMin.innerHTML = `Min year of publishing: ${library.map(b => b.year).reduce((min, b) => b < min ? b : min)}`;
+        pMin.id = "pMin";
+        const pMax = document.createElement("p");
+        pMax.innerHTML = `Max year of publishing: ${library.map(b => b.year).reduce((max, b) => b > max ? b : max)}`;
+        pMax.id = "pMax";
+        const pAverage = document.createElement("p");
+        pAverage.innerHTML = `Average year of publishing: ${Math.round(library.reduce((acc, b) => acc + b.year, 0) / library.length)}`;
+        pAverage.id = "pAverage";
+        const pNumbers = document.createElement("p");
+        pNumbers.innerHTML = `Number of books: ${library.length}`;
+        pNumbers.id = "pNumbers";
+        stats.append(pMin, pMax, pAverage, pNumbers);
+    } else {
+        pMin.innerHTML = `Min year of publishing: ${library.map(b => b.year).reduce((min, b) => b < min ? b : min)}`;
+        pMax.innerHTML = `Max year of publishing: ${library.map(b => b.year).reduce((max, b) => b > max ? b : max)}`;
+        pAverage.innerHTML = `Average year of publishing: ${Math.round(library.reduce((acc, b) => acc + b.year, 0) / library.length)}`;
+        pNumbers.innerHTML = `Number of books: ${library.length}`;
+
+    }
+}
