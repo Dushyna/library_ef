7576493cc49b229ae0e7ca5453351024b931e70c
Dushyna <olgavnv777@gmail.com>
2025-10-08 17:52:47 +0200

    refactor

--- utils.js
+++ utils.js
@@ -13,7 +13,7 @@ function createButtonDel(callback) {
 }
 
 function statusb() {
-    console.log(stats.children.length)
+    if (library.length) {
     if (stats.children.length === 1) {
         const pMin = document.createElement("p");
         pMin.innerHTML = `Min year of publishing: ${library.map(b => b.year).reduce((min, b) => b < min ? b : min)}`;
@@ -29,17 +29,20 @@ function statusb() {
         pNumbers.id = "pNumbers";
         stats.append(pMin, pMax, pAverage, pNumbers);
     } else {
-        if (library.length > 0) {
+
             pMin.innerHTML = `Min year of publishing: ${library.map(b => b.year).reduce((min, b) => b < min ? b : min)}`;
             pMax.innerHTML = `Max year of publishing: ${library.map(b => b.year).reduce((max, b) => b > max ? b : max)}`;
             pAverage.innerHTML = `Average year of publishing: ${Math.round(library.reduce((acc, b) => acc + b.year, 0) / library.length)}`;
-        } else {
+           pNumbers.innerHTML = `Number of books: ${library.length}`;
+
+    } }else {
             pMin.innerHTML = `Min year of publishing: 0`;
             pMax.innerHTML = `Max year of publishing: 0`;
             pAverage.innerHTML = `Average year of publishing: 0`;
+            pNumbers.innerHTML = `Number of books: ${library.length}`;
+
         }
-        pNumbers.innerHTML = `Number of books: ${library.length}`;
 
 
-    }
+
 }
